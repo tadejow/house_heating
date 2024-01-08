2f93140a5ae9940806e9d35bd779c3bac8f88ce9
tadejow <maciek.tadej@gmail.com>
2024-01-09 00:32:02 +0100

    new feature
    first version of the evolution function was introduced in main.py with corresponding auxiliarily function in utils

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tqdm
 import utils
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,10 +9,12 @@ class HeatingModel:
     def __init__(self, params):
         # load the model parameters
         self.params = params
-        # initialize the mini domains
+        # initialize the part domains
         self.partial_matrix = {}
-        # initialize the maxi domain
+        # initialize the full domain
         self.result_matrix = np.zeros((100, 100))
+        # initialize the mask matrix
+        self.mask_matrix = np.zeros((100, 100))
 
     def build_partial_matrix(self):
         for key in self.params["areas"].keys():
@@ -21,39 +24,59 @@ class HeatingModel:
                     self.params["areas"][key]["col_max"] - self.params["areas"][key]["col_min"]
                 )
             )
-            self.partial_matrix[key] = self.params["areas"][key]["mask"]
         return self
 
     def set_initial_data(self):
+        if "current_time" not in self.params.keys():
+            self.params["current_time"] = 0.0
         for key in self.params["areas"].keys():
             self.partial_matrix[key] = self.params["areas"][key]["init_func"](
-                self.params["domain"][
+                self.params["domain"]["grid"][
                     self.params["areas"][key]["row_min"]:self.params["areas"][key]["row_max"],
                     self.params["areas"][key]["col_min"]:self.params["areas"][key]["col_max"]
                 ]
             )
         return self
 
-    def evolve_in_unit_timestep(self, dt, dx):
-        # inside every area
-        force_term_full = self.params["force_term"](self.params["domain"])
+    def evolve_in_unit_timestep(self, dt):
+        force_term_full = self.params["force_term"](self.params["domain"]["grid"],
+                                                    self.params["current_time"],
+                                                    self.mask_matrix)
         for key in self.params["areas"].keys():
-            # diffusion effect
-            self.partial_matrix[key][1:-1, 1:-1] = dt * self.params["diffusion_coefficient"] * (
-                self.partial_matrix[key][:-2, 1:-1] + self.partial_matrix[key][2:, 1:-1] +
-                self.partial_matrix[key][1:-1, :-2] + self.partial_matrix[key][1:-1, 2:] -
-                4 * self.partial_matrix[key][1:-1, 1:-1]
-            ) / dx**2
-            # force term effect
-            self.partial_matrix[key] += dt * force_term_full[
-                        self.params["areas"][key]["row_min"]:self.params["areas"][key]["row_max"],
-                        self.params["areas"][key]["col_min"]:self.params["areas"][key]["col_max"]
-            ]
-            # boundaries
-            self.partial_matrix[key][0, :] = self.partial_matrix[key][1, :]
-            self.partial_matrix[key][-1, :] = self.partial_matrix[key][-2, :]
-            self.partial_matrix[key][:, 0] = self.partial_matrix[key][:, 1]
-            self.partial_matrix[key][:, -1] = self.partial_matrix[key][:, -2]
+            self.partial_matrix[key] = utils.single_timestep_in_evolution(
+                self.partial_matrix[key], dt, self.params["domain"]["dx"], self.params["diffusion_coefficient"],
+                force_term_full[
+                    self.params["areas"][key]["row_min"]: self.params["areas"][key]["row_max"],
+                    self.params["areas"][key]["col_min"]: self.params["areas"][key]["col_max"]
+                ]
+            )
+        for key in self.params["walls"].keys():
+            if self.params["walls"][key]["row_max"] - self.params["walls"][key]["row_min"] == 2:
+                self.result_matrix[
+                    self.params["walls"][key]["row_min"]: self.params["walls"][key]["row_max"],
+                    self.params["walls"][key]["col_min"]: self.params["walls"][key]["col_max"]
+                ] = np.mean(
+                    self.result_matrix[
+                        self.params["walls"][key]["row_min"]: self.params["walls"][key]["row_max"],
+                        self.params["walls"][key]["col_min"]: self.params["walls"][key]["col_max"]
+                    ], axis=0
+                )
+            else:
+                self.result_matrix[
+                    self.params["walls"][key]["row_min"]: self.params["walls"][key]["row_max"],
+                    self.params["walls"][key]["col_min"]: self.params["walls"][key]["col_max"]
+                ] = np.mean(
+                    self.result_matrix[
+                        self.params["walls"][key]["row_min"]: self.params["walls"][key]["row_max"],
+                        self.params["walls"][key]["col_min"]: self.params["walls"][key]["col_max"]
+                    ], axis=1
+                )
+        return self
+
+    def evolve(self, n_steps, dt):
+        for _ in tqdm.tqdm(range(n_steps), desc="TIME STEPS"):
+            self.evolve_in_unit_timestep(dt)
+            self.params["current_time"] += dt
         return self
 
     def build_result_matrix(self):
@@ -62,6 +85,12 @@ class HeatingModel:
                 self.params["areas"][key]["row_min"]:self.params["areas"][key]["row_max"],
                 self.params["areas"][key]["col_min"]:self.params["areas"][key]["col_max"]
             ] = self.partial_matrix[key]
+        if self.mask_matrix.sum() == 0.0:
+            for key in self.params["radiators"].keys():
+                self.mask_matrix[
+                    self.params["radiators"][key]["row_min"]: self.params["radiators"][key]["row_max"],
+                    self.params["radiators"][key]["col_min"]: self.params["radiators"][key]["col_max"]
+                ] = self.params["radiators"][key]["mask_value"]
         return self
 
     def build_image_frame(self):
@@ -81,28 +110,36 @@ if __name__ == '__main__':
     model_parameters = {
         "areas": {
             "A1": {
-                "row_min": 0, "row_max": 33, "col_min": 0, "col_max": 50, "mask": 1, "init_func": lambda x: -100
+                "row_min": 0, "row_max": 33, "col_min": 0, "col_max": 50,
+                "init_func": lambda x: 285 + np.random.random(x.shape)
             },
             "A2": {
-                "row_min": 0, "row_max": 27, "col_min": 50, "col_max": 100, "mask": 1, "init_func": lambda x: 200
+                "row_min": 0, "row_max": 27, "col_min": 50, "col_max": 100,
+                "init_func": lambda x: 287 + np.random.random(x.shape)
             },
             "A3": {
-                "row_min": 27, "row_max": 55, "col_min": 50, "col_max": 100, "mask": 1, "init_func": lambda x: 300
+                "row_min": 27, "row_max": 55, "col_min": 50, "col_max": 100,
+                "init_func": lambda x: 287 + np.random.random(x.shape)
             },
             "A4": {
-                "row_min": 33, "row_max": 66, "col_min": 0, "col_max": 33, "mask": 1, "init_func": lambda x: 400
+                "row_min": 33, "row_max": 66, "col_min": 0, "col_max": 33,
+                "init_func": lambda x: 291 + np.random.random(x.shape)
             },
             "A5": {
-                "row_min": 66, "row_max": 100, "col_min": 0, "col_max": 50, "mask": 1, "init_func": lambda x: 500
+                "row_min": 66, "row_max": 100, "col_min": 0, "col_max": 50,
+                "init_func": lambda x: 288 + np.random.random(x.shape)
             },
             "A6.1": {
-                "row_min": 33, "row_max": 66, "col_min": 33, "col_max": 50, "mask": 1, "init_func": lambda x: 600
+                "row_min": 33, "row_max": 66, "col_min": 33, "col_max": 50,
+                "init_func": lambda x: 290 + np.random.random(x.shape)
             },
             "A6.2": {
-                "row_min": 55, "row_max": 100, "col_min": 50, "col_max": 60, "mask": 1, "init_func": lambda x: 600
+                "row_min": 55, "row_max": 100, "col_min": 50, "col_max": 60,
+                "init_func": lambda x: 290 + np.random.random(x.shape)
             },
             "A7": {
-                "row_min": 55, "row_max": 100, "col_min": 60, "col_max": 100, "mask": 0, "init_func": lambda x: 100
+                "row_min": 55, "row_max": 100, "col_min": 60, "col_max": 100,
+                "init_func": lambda x: 275 + np.random.random(x.shape)
             }
         },
         "walls": {
@@ -160,7 +197,34 @@ if __name__ == '__main__':
                 "row_min": 99, "row_max": 100, "col_min": 15, "col_max": 35
             }
         },
-        "domain": np.meshgrid(np.linspace(-1, 1, 101), np.linspace(-1, 1, 101))[0]
+        "radiators": {
+            "R1": {
+                "row_min": 2, "row_max": 3, "col_min": 17, "col_max": 33, "mask_value": 1
+            },
+            "R2": {
+                "row_min": 2, "row_max": 3, "col_min": 52, "col_max": 66, "mask_value": 2
+            },
+            "R3": {
+                "row_min": 35, "row_max": 43, "col_min": 30, "col_max": 31, "mask_value": 3
+            },
+            "R4": {
+                "row_min": 97, "row_max": 98, "col_min": 17, "col_max": 33, "mask_value": 4
+            }
+        },
+        "domain": {
+            "grid": np.meshgrid(np.linspace(-1, 1, 101), np.linspace(-1, 1, 101))[0], "dx": 1
+        },
+        "force_term": lambda x, t, mask: np.where(
+            mask == 1, (np.cos(t)**2 + 0) * 10**(-1), np.where(
+                mask == 2, (np.cos(t)**2 + 0) * 10**(-1), np.where(
+                    mask == 3, (np.cos(t)**2 + 0) * 10**(-1), np.where(
+                        mask == 4, (np.cos(t)**2 + 0) * 10**(-1), 0
+                    )
+                )
+            )
+        ) * 10**(-4),
+        "diffusion_coefficient": 10**(-5)
+
     }
     model = HeatingModel(model_parameters)
     model.build_partial_matrix()
@@ -168,5 +232,11 @@ if __name__ == '__main__':
     model.build_result_matrix()
     plt.imshow(model.result_matrix)
     plt.show()
-    model.build_image_frame().show()
+    # model.build_image_frame().show()
+    # plt.show()
+    model.evolve(10, 0.01)
+    model.build_result_matrix()
+    plt.imshow(model.result_matrix)
     plt.show()
+    # model.build_image_frame().show()
+    # plt.show()
--- utils.py
+++ utils.py
@@ -9,3 +9,21 @@ def grayscale_array_to_coolwarm_image(matrix):
     rgb_image = Image.new("RGBA", image.size)
     rgb_image.paste(image)
     return rgb_image
+
+
+def single_timestep_in_evolution(matrix, dt, dx, diffusion_coefficient, force_term):
+    # diffusion effect
+    matrix[1:-1, 1:-1] = dt * diffusion_coefficient * (
+            matrix[:-2, 1:-1] + matrix[2:, 1:-1] + matrix[1:-1, :-2] + matrix[1:-1, 2:] - 4 * matrix[1:-1, 1:-1]
+    ) / dx ** 2
+    # force term effect
+    matrix += dt * force_term
+    # boundaries
+    matrix[0, :] = matrix[1, :]
+    matrix[-1, :] = matrix[-2, :]
+    matrix[:, 0] = matrix[:, 1]
+    matrix[:, -1] = matrix[:, -2]
+    return matrix
+
+def single_timestep_matrix_merge(matrix, axis):
+    return np.sum(matrix, axis)
