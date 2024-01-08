ac5bfcd5bc575f3a7c45d0b22c382d2004022469
tadejow <maciek.tadej@gmail.com>
2024-01-08 20:41:33 +0100

    logic refactor
    Changed the logic of constructing the spatial domain

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import itertools
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -45,11 +43,20 @@ class HeatingModel:
         return self
 
     def build_image_frame(self):
-        image = Image.fromarray(np.uint8(self.result_matrix))
+        cm = plt.get_cmap("coolwarm")
+        image = Image.fromarray(np.uint8(cm(self.result_matrix / np.max(self.result_matrix))[:, :, :3] * 255))
+        rgb_image = Image.new("RGBA", image.size)
+        rgb_image.paste(image)
+        image = rgb_image
+        del rgb_image
         for key in self.params["walls"].keys():
             for p1 in range(self.params["walls"][key]["row_min"], self.params["walls"][key]["row_max"]):
                 for p2 in range(self.params["walls"][key]["col_min"], self.params["walls"][key]["col_max"]):
-                    image.putpixel((p1, p2), 0)
+                    image.putpixel((p2, p1), (0, 0, 0))
+        for key in self.params["windows"].keys():
+            for p1 in range(self.params["windows"][key]["row_min"], self.params["windows"][key]["row_max"]):
+                for p2 in range(self.params["windows"][key]["col_min"], self.params["windows"][key]["col_max"]):
+                    image.putpixel((p2, p1), (0, 0, 255))
         return image
 
 
@@ -57,39 +64,84 @@ if __name__ == '__main__':
     model_parameters = {
         "areas": {
             "A1": {
-                "row_min": 0, "row_max": 35, "col_min": 0, "col_max": 55, "mask": 1, "init_func": lambda x: 100
+                "row_min": 0, "row_max": 33, "col_min": 0, "col_max": 50, "mask": 1, "init_func": lambda x: -100
             },
             "A2": {
-                "row_min": 0, "row_max": 35, "col_min": 55, "col_max": 100, "mask": 0, "init_func": lambda x: 100
+                "row_min": 0, "row_max": 27, "col_min": 50, "col_max": 100, "mask": 1, "init_func": lambda x: 200
             },
             "A3": {
-                "row_min": 35, "row_max": 65, "col_min": 0, "col_max": 55, "mask": 1, "init_func": lambda x: 200
+                "row_min": 27, "row_max": 55, "col_min": 50, "col_max": 100, "mask": 1, "init_func": lambda x: 300
             },
             "A4": {
-                "row_min": 50, "row_max": 80, "col_min": 55, "col_max": 100, "mask": 1, "init_func": lambda x: 300
+                "row_min": 33, "row_max": 66, "col_min": 0, "col_max": 33, "mask": 1, "init_func": lambda x: 400
             },
             "A5": {
-                "row_min": 65, "row_max": 100, "col_min": 0, "col_max": 55, "mask": 1, "init_func": lambda x: 400
+                "row_min": 66, "row_max": 100, "col_min": 0, "col_max": 50, "mask": 1, "init_func": lambda x: 500
             },
-            "A6": {
-                "row_min": 65, "row_max": 100, "col_min": 55, "col_max": 100, "mask": 1, "init_func": lambda x: 500
+            "A6.1": {
+                "row_min": 33, "row_max": 66, "col_min": 33, "col_max": 50, "mask": 1, "init_func": lambda x: 600
+            },
+            "A6.2": {
+                "row_min": 55, "row_max": 100, "col_min": 50, "col_max": 60, "mask": 1, "init_func": lambda x: 600
+            },
+            "A7": {
+                "row_min": 55, "row_max": 100, "col_min": 60, "col_max": 100, "mask": 0, "init_func": lambda x: 100
             }
         },
         "walls": {
-            "VV1": {"row_min": 0, "row_max": 35, "col_min": 0, "col_max": 55},
-            "VV2": {"row_min": 0, "row_max": 35, "col_min": 55, "col_max": 100},
-            "VV3": {"row_min": 35, "row_max": 65, "col_min": 0, "col_max": 55},
-            "VV4": {"row_min": 50, "row_max": 80, "col_min": 55, "col_max": 100},
-            "VV5": {"row_min": 65, "row_max": 100, "col_min": 0, "col_max": 55},
-            "VV6": {"row_min": 65, "row_max": 100, "col_min": 55, "col_max": 100}
+            "VV1": {
+                "row_min": 0, "row_max": 100, "col_min": 0, "col_max": 1
+            },
+            "VV2": {
+                 "row_min": 0, "row_max": 1, "col_min": 0, "col_max": 100
+            },
+            "VV3": {
+                 "row_min": 0, "row_max": 100, "col_min": 99, "col_max": 100
+            },
+            "VV4": {
+                "row_min": 99, "row_max": 100, "col_min": 0, "col_max": 100
+            },
+            "VV5": {
+                "row_min": 0, "row_max": 55, "col_min": 49, "col_max": 51
+            },
+            "VV6": {
+                "row_min": 26, "row_max": 28, "col_min": 59, "col_max": 100
+            },
+            "VV7": {
+                "row_min": 32, "row_max": 34, "col_min": 0, "col_max": 34
+            },
+            "VV8": {
+                "row_min": 33, "row_max": 45, "col_min": 32, "col_max": 34
+            },
+            "VV9": {
+                "row_min": 55, "row_max": 66, "col_min": 32, "col_max": 34
+            },
+            "VV10": {
+                "row_min": 65, "row_max": 67, "col_min": 0, "col_max": 34
+            },
+            "VV11": {
+                "row_min": 65, "row_max": 100, "col_min": 49, "col_max": 51
+            },
+            "VV12": {
+                "row_min": 55, "row_max": 100, "col_min": 59, "col_max": 61
+            },
+            "VV13": {
+                "row_min": 54, "row_max": 56, "col_min": 59, "col_max": 100
+            }
         },
         "windows": {
-            "W1": {"row_min": 0, "row_max": 35, "col_min": 0, "col_max": 55},
-            "W2": {"row_min": 0, "row_max": 35, "col_min": 55, "col_max": 100},
-            "W3": {"row_min": 35, "row_max": 65, "col_min": 0, "col_max": 55},
-            "W4": {"row_min": 50, "row_max": 80, "col_min": 55, "col_max": 100},
-            "W5": {"row_min": 65, "row_max": 100, "col_min": 0, "col_max": 55},
-            "W6": {"row_min": 65, "row_max": 100, "col_min": 55, "col_max": 100}
+            "W1": {
+                "row_min": 0, "row_max": 1, "col_min": 15, "col_max": 35
+            },
+            "W2": {
+                "row_min": 0, "row_max": 1, "col_min": 70, "col_max": 90
+            },
+            "W3": {
+                "row_min": 30, "row_max": 40, "col_min": 99, "col_max": 100
+            },
+            "W4": {
+                "row_min": 99, "row_max": 100, "col_min": 15, "col_max": 35
+            }
         },
         "domain": np.meshgrid(np.linspace(-1, 1, 101), np.linspace(-1, 1, 101))[0]
     }
